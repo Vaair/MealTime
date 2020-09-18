c0b80468422934d3abe518631670c90541286c1b
Vaair <valeria.tarasenko.a@gmail.com>
2020-09-18 13:42:58 +0300

    add model

---
+++ CoreData entities/Meal+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Лера Тарасенко on 18.09.2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
